8de0aaefbb3f734f08e3ab43bed41574c94e8ae6
hi.tuhingt@gmail.com <hi.tuhingt@gmail.com>
2024-07-24 12:30:34 -0400

    added multiply.java

---
+++ LearnGit/Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        int a= 5;
+        int b=4;
+        System.out.println("Multiply of "+a+" + "+b+" is:"+a*b);
+    }
+}
